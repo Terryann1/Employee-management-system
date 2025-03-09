a8ae0230096bc6dd573268aa95ca24b8f0a3b63a
+ <+>
2025-03-09 22:27:41 +0300

    created relationship between tables

--- models.py
+++ models.py
@@ -1,9 +1,17 @@
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, String, ForeignKey,Table
 from sqlalchemy.orm import relationship
 
 
 Base=declarative_base()
+
+
+#creating an association table
+employee_project = Table(
+    'employee_project', Base.metadata,
+    Column('employee_id', Integer, ForeignKey('employees.id')),
+    Column('project_id', Integer, ForeignKey('projects.id'))
+)
 
 
 #Creating the employee model
@@ -18,6 +26,14 @@ class Employee(Base):
     #Adding a foreign key
     project_id=Column(Integer,ForeignKey('projects.id'))
     department_id=Column(Integer,ForeignKey('departments.id'))
+
+    #one-to-many relationship
+    department = relationship("Department", backref="employees")
+
+      # Many-to-Many relationship with Project
+    projects = relationship("Project", secondary=employee_project, back_populates="employees")
+    
+
    
 
 # creating the department model
@@ -30,4 +46,10 @@ class Department(Base):
 class Project(Base):
     __tablename__="projects"
     id=Column(Integer,primary_key=True)
-    name=Column(String,nullable=False)   
+    name=Column(String,nullable=False) 
+
+    # Many-to-Many relationship with Employee
+    employees = relationship("Employee", secondary=employee_project, back_populates="projects") 
+
+
+ 
